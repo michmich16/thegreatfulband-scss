cb6e30dc90ee23777800af841dd9cf6f26927a4e
Michael P <102651473+michmich16@users.noreply.github.com>
2024-05-29 10:54:48 +0200

    push

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menuBtn = document.getElementById('menu-btn');
     const navMenu = document.getElementById('nav-menu');
+    const body = document.body;
 
     menuBtn.addEventListener('click', () => {
         menuBtn.classList.toggle('open');
         navMenu.classList.toggle('open');
+        body.classList.toggle('noscroll');
     });
 });
